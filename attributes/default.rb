fcb882092fb423d38d0f449b76252f116fafb7de
Aaron Collins <acollins@chegg.com>
2016-04-04 18:58:21 -1000

    inital commit

---
+++ attributes/default.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+# instana-agent - Attributes file - defining attributes for the instana-agent
+default['instana']['tenant']['name'] = 'Your tenant name'
+default['instana']['tenant']['unit'] = 'Your tenant unit name'
+default['instana']['tenant']['email'] = 'your@email.address'
+default['instana']['tenant']['password'] = 'Y0urP4ssw0rd'
+default['instana']['tenant']['agentSrc'] = '/tmp/instana-agent.tgz'
+default['instana']['tenant']['agentDest'] = '/opt/instana-agent'
+# node.default['javaloc'] = '/opt/jdk1.8path'
